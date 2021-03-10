d9d95db557dec6990354899ebc041553118ded81
Milo Silva <milo.silva234@gmail.com>
2021-03-10 16:10:36 +0100

    Clean up comments and code

--- routers/characters.js
+++ routers/characters.js
@@ -1,11 +1,9 @@
 const { default: axios } = require("axios");
 const { apiUrl } = require("../config/constants");
 const router = require("express").Router();
-const Cache = require("../lib/cache");
-const fetchCharacter = require("../lib/fetchCharacter");
+const retrieveFromCache = require("../lib/retrieveFromCache");
 const { sortByAge, sortByHeight } = require("../lib/sortBy");
-const charCache = new Cache(0);
-// assume api should return all characters from all movies that match the search term used. ()
+// assume api should return all characters from all movies that match the search term used.
 // assume if multiple movies match a search term, characters from all movies should be combined (duplicates removed).
 // since every character includes a list of the films they were in,
 // Can use this information in FE to re-categorise characters by their movie.
@@ -14,10 +12,10 @@ const charCache = new Cache(0);
     {
       page: number
       count: number
-      matchedMovies: title[]
+      matchedMovies: title[] // use this & character.films to re-match characters to films in FE
       characters: character[] limit 30 results
     }
-  */
+*/
 
 router.get("/", async (req, res, next) => {
   const { page = 0, title, gender, sortby, order = "descending" } = req.query;
@@ -34,31 +32,21 @@ router.get("/", async (req, res, next) => {
       data: { results },
     } = await axios.get(`${apiUrl}/films/?search=${title}`);
 
-    // check if there are search matches
+    // check if any film matches the search term
     if (!results || results.length < 1)
-      return res.status(200).send({ message: "No movies Match the search term" });
+      return res.status(200).send({ message: "No movies match the search term" });
 
-    // combine characters from all movies matched into single array
+    // combine all characters
     let combinedCharacters = [];
     results.forEach((movie) => (combinedCharacters = combinedCharacters.concat(movie.characters)));
 
-    // remove duplacate characters from the character list
+    // remove duplicate characters
     const uniqueCharactersUrls = Array.from(new Set(combinedCharacters));
-    // retrieve all people from api (w/ 9 requests via pagination & cache results)
-    // OR retrieve characters individually as required (& cache the results)
 
-    // create a list of promises and resolve them.
-    let uniqueCharactersList = await Promise.all(
-      uniqueCharactersUrls.map((urlString) => {
-        // get the character Ids from the url string
-        const characterId = urlString.match(/[0-9]+/)[0];
+    // create and resolve list of promises
+    let uniqueCharactersList = await retrieveFromCache(uniqueCharactersUrls, "people");
 
-        // use the cache to check if value is already stored otherwise, make a request
-        return charCache.get(characterId, fetchCharacter);
-      })
-    );
-
-    // check & filter by gender
+    // check for & filter by gender
     if (gender)
       uniqueCharactersList = uniqueCharactersList.filter(
         (character) => character.gender === gender
@@ -72,7 +60,8 @@ router.get("/", async (req, res, next) => {
         uniqueCharactersList = uniqueCharactersList.sort(sortByHeight(order));
       }
     }
-    // paginate the results
+
+    // paginate results
     const count = uniqueCharactersList.length;
     const startingIndex = Number(page) * 30;
     const matchedMovies = results.map((movie) => movie.title);
@@ -81,7 +70,6 @@ router.get("/", async (req, res, next) => {
       Math.min(startingIndex + 30, count)
     );
 
-    // send the results back
     res.json({ page, count, matchedMovies, characters: paginatedResults });
   } catch (e) {
     console.log(e.message);
