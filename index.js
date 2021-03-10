5f58f3acaf6d5d9ca72e98348f65ffa96bfe4df2
Milo Silva <milo.silva234@gmail.com>
2021-03-10 16:01:51 +0100

    Feat 2 planets by climate type endpoint - add planet router to server entry file - create working planets endpoint with collection of dark haired residents for each planet - hard code hair_colour - export shared cache instance from cache.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const corsInstance = cors();
 const logMiddleware = require("morgan");
 const jsonParser = express.json();
+const cache = require("./lib/cache");
 
 // Middleware
 app.use(corsInstance);
@@ -12,12 +13,15 @@ app.use(logMiddleware("dev")); // level of verboseness
 
 // Import Routers
 const charactersRouter = require("./routers/characters");
+const planetsRouter = require("./routers/planets");
 
 // Routes
 app.use("/characters", charactersRouter);
+app.use("/planets", planetsRouter);
 
-// test route
+// test routes
 app.all("/testytest", (_, res) => res.send("hello"));
+app.all("/cache-stats", (_, res) => res.send(cache.stats()));
 
 // start server process
 const internalIp = require("internal-ip").v4.sync();
--- routers/planets.js
+++ routers/planets.js
@@ -1,27 +1,60 @@
 const { default: axios } = require("axios");
 const { apiUrl } = require("../config/constants");
 const router = require("express").Router();
+const Cache = require("../lib/cache");
+const fetchAll = require("../lib/fetchAllPages");
+const retrieveFromCache = require("../lib/retrieveFromCache");
+// endpoint that return an *object* of all the planets that have this climate-type.
+// this planet object includes a collection of all dark-haired characters that live on the planet.
+// assume dark-hair is either brown or black hair.
+const hair_colour = ["black", "brown"];
 
-// endpoint that return an object of all the planets that have this climate-type.
-// object includes a collection of all dark-haired characters that live on the planet.
-
-/* schema 
+/* Schema 
   {
+    ...
     "planetName": dark-haired-character[]
+    ...
   }
 */
 
-router.get("/:climateType", async (req, res, next) => {
-  const { climateType } = req.params;
-  const { hair_type = "black" } = req.query;
+router.get("/climate/:climateType", async (req, res, next) => {
+  const { climateType = "" } = req.params;
+  console.log("climate type:", climateType);
+  try {
+    // fetch all of the planets from cache or req
+    // planets stored in cache under a single key
+    // (not best approach, but fits requirements)
+    const allPLanets = await Cache.get("planets", fetchAll);
 
-  // fetch all planets
-  // then: filter planets by climate type
-  // then: fetch all residents
-  // then: filter residents by hair_type
-  console.log("climateType:", climateType);
+    // filter planets by climate type
+    const filteredPlanetsList = allPLanets.filter((planet) => planet.climate.includes(climateType));
 
-  res.json();
+    const planets = {};
+    // use Promise.all to suspend fn execution
+    // until promises are resolved
+    await Promise.all(
+      filteredPlanetsList.map(async (planet) => {
+        // retrieve character data from cache/request
+        const residents = await retrieveFromCache(planet.residents);
+
+        // filter residents by hair type
+        const darkHairedResidents = residents.filter((character) =>
+          hair_colour.reduce(
+            (prevResult, currentHair) => prevResult || character.hair_color.includes(currentHair),
+            false
+          )
+        );
+        // add all planets to the planets object
+        planets[planet.name] = { ...planet, residents, darkHairedResidents };
+        return;
+      })
+    );
+    // validate climate type
+    if (Object.keys(planets) < 1) return res.json({ message: "invalid climate type" });
+    res.json(planets);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
